8624d98568bdd94c5fe5d65181cb330a96399131
Daniel Pfeil <dp@api-studio.de>
2018-05-17 07:46:40 +0200

    extracted getAtom function

--- src/test/java/com/pfeilda/ajb/miscellaneous/TestInterface.java
+++ src/test/java/com/pfeilda/ajb/miscellaneous/TestInterface.java
@@ -3,4 +3,5 @@ package com.pfeilda.ajb.miscellaneous;
 public interface TestInterface {
     @org.junit.Test
     public void creation();
+    public Object genereateInstanceOfTestClass();
 }
--- src/test/java/com/pfeilda/ajb/particles/AtomTest.java
+++ src/test/java/com/pfeilda/ajb/particles/AtomTest.java
@@ -1,6 +1,5 @@
 package com.pfeilda.ajb.particles;
 
-import com.pfeilda.ajb.miscellaneous.Singleton;
 import com.pfeilda.ajb.miscellaneous.TestInterface;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,50 +9,51 @@ public class AtomTest implements TestInterface {
     private final String elementSymbol = "H";
     private final String label = "Wasserstoff";
 
-    public final Atom getAtom() {
+    @Override
+    public final Atom genereateInstanceOfTestClass() {
         return new Atom(this.label, this.elementSymbol, this.ordinal);
     }
 
     @Override
     @Test
     public final void creation() {
-        final Atom atom = this.getAtom();
+        final Atom atom = this.genereateInstanceOfTestClass();
         Assert.assertNotNull(atom);
     }
 
     @Test
     public final void inheritanceSuccess() {
-        final Atom atom = this.getAtom();
+        final Atom atom = this.genereateInstanceOfTestClass();
         Assert.assertNotNull((Element) atom);
     }
 
     @Test
     public final void experimentalProofSuccess() {
-        final Atom atom = this.getAtom();
+        final Atom atom = this.genereateInstanceOfTestClass();
         Assert.assertTrue(atom.experimentalProof());
     }
 
     @Test
     public final void experimentalProofFails() {
-        final Atom atom = this.getAtom();
+        final Atom atom = this.genereateInstanceOfTestClass();
         Assert.assertFalse(atom.experimentalProof());
     }
 
     @Test
     public final void getLabel() {
-        final Atom atom = this.getAtom();
+        final Atom atom = this.genereateInstanceOfTestClass();
         Assert.assertEquals(atom.getLabel(), this.label);
     }
 
     @Test
     public final void getOrdinal() {
-        final Atom atom = this.getAtom();
+        final Atom atom = this.genereateInstanceOfTestClass();
         Assert.assertEquals(atom.getOrdinal(), this.ordinal);
     }
 
     @Test
     public final void getElementSymbol() {
-        final Atom atom = this.getAtom();
+        final Atom atom = this.genereateInstanceOfTestClass();
         Assert.assertEquals(atom.getElementSymbol(), this.elementSymbol);
     }
 }
