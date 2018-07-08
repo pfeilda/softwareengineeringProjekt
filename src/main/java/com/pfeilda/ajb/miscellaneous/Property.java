d9b89419c090b1daf594d452ea5fcfc9885ddd94
Daniel Pfeil <dp@api-studio.de>
2018-07-08 23:35:09 +0200

    implemented Property

---
+++ src/main/java/com/pfeilda/ajb/miscellaneous/Property.java
@@ -0,0 +1,31 @@
+package com.pfeilda.ajb.miscellaneous;
+
+public abstract class Property {
+    private double property;
+
+    public Property(final double property) {
+        this.property = property;
+    }
+
+    public double get() {
+        return this.property;
+    }
+
+    public void add(final double property) {
+        this.property = this.property + property;
+    }
+
+    public void add(final Property property) {
+        this.add(property.get());
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this.getClass().equals(obj.getClass())) {
+            if (this.get() == ((Property) obj).get()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
