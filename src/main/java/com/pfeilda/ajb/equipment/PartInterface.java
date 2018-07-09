cbb9965a744784fd71e8dd31f884153b7fc2634c
Daniel Pfeil <dp@api-studio.de>
2018-07-09 19:05:47 +0200

    [REFACTOR] changed class inheritance

--- src/main/java/com/pfeilda/ajb/equipment/PartInterface.java
+++ src/main/java/com/pfeilda/ajb/equipment/PartInterface.java
@@ -1,7 +1,5 @@
 package com.pfeilda.ajb.equipment;
 
-import com.pfeilda.ajb.miscellaneous.AlterInterface;
-
-public interface PartInterface extends AlterInterface {
+public interface PartInterface {
     public void add(SubstanceContainer substanceContainer);
 }
