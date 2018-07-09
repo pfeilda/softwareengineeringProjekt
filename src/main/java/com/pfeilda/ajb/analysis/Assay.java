285e1597f0fad170e47a3165a1aae0e9646cb236
Daniel Pfeil <dp@api-studio.de>
2018-07-09 19:01:31 +0200

    Implemented function elementIsPresent in Assay

--- src/main/java/com/pfeilda/ajb/analysis/Assay.java
+++ src/main/java/com/pfeilda/ajb/analysis/Assay.java
@@ -3,12 +3,16 @@ package com.pfeilda.ajb.analysis;
 import com.pfeilda.ajb.particles.Element;
 
 import java.util.List;
+import java.util.Random;
 
 public abstract class Assay extends AbstractSubstance {
-    public Assay(final List<AbstractSubstance> abstractSubstances) {
-        super(abstractSubstances);
-    }
-
     public abstract boolean isDisruptiveElementPresent(Element element);
 
+    public boolean elementIsPresent(final Element element) {
+        if (!this.isDisruptiveElementPresent(element)) {
+            return this.elements.contains(element);
+        }
+        final Random random = new Random();
+        return random.nextBoolean();
+    }
 }
