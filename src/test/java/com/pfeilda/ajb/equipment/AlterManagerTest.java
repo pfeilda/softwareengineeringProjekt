348e5a63cc25600330a9fa64d39d8b51fe2aa403
Daniel Pfeil <dp@api-studio.de>
2018-07-10 14:27:26 +0200

    [BUGFIX]added missing test annotation

--- src/test/java/com/pfeilda/ajb/equipment/AlterManagerTest.java
+++ src/test/java/com/pfeilda/ajb/equipment/AlterManagerTest.java
@@ -11,6 +11,7 @@ public class AlterManagerTest implements TestInterface {
     }
 
     @Override
+    @Test
     public void creation() {
         Assert.assertNotNull(this.generateInstanceOfTestClass());
     }
