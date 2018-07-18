ec644f025ee51de606d93ffd71ba4b4e9ae08cd4
Daniel Pfeil <dp@api-studio.de>
2018-07-18 23:18:53 +0200

    added Test annotation

--- src/test/java/com/pfeilda/ajb/equipment/ExternalReagentTest.java
+++ src/test/java/com/pfeilda/ajb/equipment/ExternalReagentTest.java
@@ -2,6 +2,7 @@ package com.pfeilda.ajb.equipment;
 
 import com.pfeilda.ajb.miscellaneous.TestInterface;
 import org.junit.Assert;
+import org.junit.Test;
 
 public abstract class ExternalReagentTest implements TestInterface {
     public abstract ExternalReagent generateInstanceOfTestClass(ExternalReagent externalReagent);
@@ -9,12 +10,14 @@ public abstract class ExternalReagentTest implements TestInterface {
     @Override
     public abstract ExternalReagent generateInstanceOfTestClass();
 
+    @Test
     public void getParentSelf() {
         final ExternalReagent parentExternalReagent = this.generateInstanceOfTestClass();
         final ExternalReagent childrenExternalReagent = this.generateInstanceOfTestClass(parentExternalReagent);
         Assert.assertEquals(parentExternalReagent, childrenExternalReagent.getParent());
     }
 
+    @Test
     public void getParentOther() {
         final ExternalReagent parentExternalReagent = this.generateInstanceOfTestClass();
         final ExternalReagent childrenExternalReagent = this.generateInstanceOfTestClass(parentExternalReagent);
