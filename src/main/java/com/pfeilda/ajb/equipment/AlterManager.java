fbd030f414a029c7f24fc3d26b5fb3224012b682
Daniel Pfeil <dp@api-studio.de>
2018-07-18 23:13:09 +0200

    implemented speed value for AlterManager

--- src/main/java/com/pfeilda/ajb/equipment/AlterManager.java
+++ src/main/java/com/pfeilda/ajb/equipment/AlterManager.java
@@ -1,10 +1,14 @@
 package com.pfeilda.ajb.equipment;
 
+import com.pfeilda.ajb.miscellaneous.Log;
+
 import java.util.Observable;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AlterManager extends Observable implements Runnable {
     private static volatile AlterManager instance;
     private static final Object mutex = new Object();
+    private final AtomicInteger speed = new AtomicInteger(100);
 
     private AlterManager() {
     }
@@ -25,7 +29,21 @@ public class AlterManager extends Observable implements Runnable {
     @Override
     public void run() {
         while (true) {
+            this.setChanged();
             this.notifyObservers();
+            try {
+                Thread.sleep(this.speed.get());
+            } catch (final InterruptedException e) {
+                Log.log(this.getClass().getName(), e, "Failed Sleep For Altering.");
+            }
         }
+    }
+
+    public void setSpeed(final int speed) {
+        this.speed.set(speed);
+    }
+
+    public AtomicInteger getSpeed() {
+        return this.speed;
     }
 }
--- src/test/java/com/pfeilda/ajb/equipment/AlterManagerTest.java
+++ src/test/java/com/pfeilda/ajb/equipment/AlterManagerTest.java
@@ -4,6 +4,8 @@ import com.pfeilda.ajb.miscellaneous.TestInterface;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AlterManagerTest implements TestInterface {
     @Override
     public AlterManager generateInstanceOfTestClass() {
@@ -14,5 +16,11 @@ public class AlterManagerTest implements TestInterface {
     @Test
     public void creation() {
         Assert.assertNotNull(this.generateInstanceOfTestClass());
+    }
+
+    @Test
+    public void getInitialSpeedValue() {
+        final AlterManager alterManager = this.generateInstanceOfTestClass();
+        Assert.assertEquals(new AtomicInteger(100).get(), alterManager.getSpeed().get());
     }
 }
